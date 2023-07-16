66bc9807795978aa813ba70f0f6073bdd94c662f
benlee <bentl22a9@gmail.com>
2023-07-16 13:54:23 +0900

    feat: add modal

--- piggy-bank-demo-react/src/pages/UserDemo/index.tsx
+++ piggy-bank-demo-react/src/pages/UserDemo/index.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
-import { Box, Spacer, Text, VStack, keyframes, Tag } from '@chakra-ui/react';
+import React, { useCallback, useState } from 'react';
+import {
+  Box,
+  Spacer,
+  Text,
+  VStack,
+  Tag,
+  Modal,
+  keyframes,
+  useDisclosure,
+  ModalOverlay,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  ModalContent,
+  ModalHeader,
+  Button
+} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { SectionCard } from '../../components';
 
@@ -17,6 +33,15 @@ const animationX = `${keyframesX} 3s linear infinite alternate`;
 const animationY = `${keyframesY} 1.2s linear infinite alternate`;
 
 const UserDemo = () => {
+  // const [showModal, setShowModal] = useState<boolean>(false);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const onPiggyClick = useCallback(() => {
+    // setShowModal(true);
+    onOpen();
+  }, []);
+
   return (
     <VStack w="100%" h="100%">
       <Spacer />
@@ -26,7 +51,7 @@ const UserDemo = () => {
           <Box w="100%" h="200px">
             <Box as={motion.div} animation={animationX}>
               <Box w="80px" h="80px" as={motion.div} animation={animationY}>
-                <VStack>
+                <VStack onClick={() => onPiggyClick()}>
                   <Text fontSize={'4xl'}>🐷</Text>
                   <Tag>catch me</Tag>
                 </VStack>
@@ -36,6 +61,21 @@ const UserDemo = () => {
         }
       />
       <Spacer />
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>dummy</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </VStack>
   );
 };
